cd2eaa49dbc78d57181e7ff15a7f759c93e4ba65
friedrich <fanders@icc.ub.edu>
2022-11-16 20:55:37 +0100

    first commit to new repo

---
+++ mcmc_binarity.py
@@ -0,0 +1,342 @@
+import numpy as np
+import os
+from astropy.table import Table, vstack
+
+from scipy import interpolate
+from scipy.stats import norm
+import scipy.optimize as op
+import time
+import emcee
+import corner
+from multiprocessing import Pool
+
+from matplotlib import pyplot as plt
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+from astroML.plotting import setup_text_plots
+setup_text_plots(fontsize=22, usetex=True)
+
+def lnlike(theta, x, y, yerr):
+    """
+    Polynomial MS + parallel binary sequence (both with constant scatter)
+    """
+    # Unpack theta: First the polynomial coeffs, then the binarity parameters
+    #p0,p1,p2,p3,p4, fb, DeltaG, sigMS, sigBS = theta
+    fb, DeltaG, sigMS, sigBS = theta[-4:] 
+    polycoeffs_ms            = np.array(theta[:-4][::-1])
+    polycoeffs_bs            = polycoeffs_ms.copy()
+    polycoeffs_bs[-1]        = polycoeffs_ms[-1] + DeltaG
+    assert polycoeffs_ms[-1] != polycoeffs_bs[-1]
+    # Define the functional form of the MS and the BS
+    mainseq   = np.poly1d(polycoeffs_ms)
+    binseq    = np.poly1d(polycoeffs_bs)
+    # Compute the likelihood - as a product of the individual data point probabilities
+    summand_1 = (1-fb) * np.exp( - (y - mainseq(x))**2. / (2.*(yerr**2. + sigMS**2.))) / np.sqrt(yerr**2. + sigMS**2.)
+    summand_2 =    fb  * np.exp( - (y -  binseq(x))**2. / (2.*(yerr**2. + sigBS**2.))) / np.sqrt(yerr**2. + sigBS**2.)
+    logL      =  np.sum(np.log(summand_1 + summand_2))
+    return logL
+
+def lnlike2(theta, x, y, xerr, yerr):
+    """
+    Polynomial MS + parallel binary sequence (both with constant scatter).
+    This takes into account uncertainties in the colour (x axis) too 
+    """
+    # Unpack theta: First the polynomial coeffs, then the binarity parameters
+    #p0,p1,p2,p3,p4, fb, DeltaG, sigMS, sigBS = theta
+    fb, DeltaG, sigMS, sigBS = theta[-4:] 
+    polycoeffs_ms            = np.array(theta[:-4][::-1])
+    polycoeffs_bs            = polycoeffs_ms.copy()
+    polycoeffs_bs[-1]        = polycoeffs_ms[-1] + DeltaG
+    assert polycoeffs_ms[-1] != polycoeffs_bs[-1]
+    # Define the functional form of the MS and the BS
+    mainseq   = np.poly1d(polycoeffs_ms)
+    binseq    = np.poly1d(polycoeffs_bs)
+    # Compute the effective sigma_x (we need the orthogonal component in each )
+    derivative= np.polyder(mainseq)
+    sigma2_eff= ( (derivative(x) * xerr)**2. + yerr**2. )/(1. + (derivative(x))**2. ) 
+    # Compute the likelihood - as a product of the individual data point probabilities
+    summand_1 = (1-fb) * np.exp( - (y - mainseq(x))**2. / (2.*(sigma2_eff + sigMS**2.))) / np.sqrt(sigma2_eff + sigMS**2.)
+    summand_2 =    fb  * np.exp( - (y -  binseq(x))**2. / (2.*(sigma2_eff + sigBS**2.))) / np.sqrt(sigma2_eff + sigBS**2.)
+    logL      =  np.sum(np.log(summand_1 + summand_2))
+    return logL
+
+def lnlike_simple(theta, x, y, yerr):
+    """
+    Polynomial MS + parallel binary sequence (both with constant scatter)
+    """
+    # Unpack theta
+    polycoeffs = theta[::-1]
+    # Define the functional form of the MS and the BS
+    mainseq   = np.poly1d(polycoeffs)
+    # Compute the likelihood - as a product of the individual data point probabilities
+    return np.sum(- (y - mainseq(x))**2. / (2.*(yerr**2.)))
+
+def lnprior(theta):
+    """
+    Return flat priors in all parameters
+    """
+    # Unpack theta: First the polynomial coeffs, then the binarity parameters
+    polycoeffs               = theta[:-4]
+    fb, DeltaG, sigMS, sigBS = theta[-4:]
+    if (abs(p) < 10 for p in polycoeffs) and \
+    0 <   fb  < 0.9 and -0.9 < DeltaG < -0.5  and \
+    0 < sigMS < 0.3 and  0   <  sigBS <  0.3 :
+        return 0.0
+    return -np.inf
+
+def lnprior2(theta):
+    """
+    Return flat priors in fb + Gaussians in DeltaG, sigMS, sigBS
+    """
+    # Unpack theta: First the polynomial coeffs, then the binarity parameters
+    polycoeffs               = theta[:-4]
+    fb, DeltaG, sigMS, sigBS = theta[-4:]
+    if 0 <  fb  < 0.9 and -1 < DeltaG < -0.4 and 0 < sigMS < 0.3 and 0 < sigBS < 0.3:
+        return norm.pdf(DeltaG, -0.75, 0.05) * \
+               norm.pdf(np.log10(sigMS), -1, 0.2) * \
+               norm.pdf(np.log10(sigBS), -0.8, 0.2)
+    return -np.inf
+
+def lnprob(theta, x, y, yerr):
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + lnlike(theta, x, y, yerr)
+
+def lnprob2(theta, x, y, xerr, yerr):
+    lp = lnprior2(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + lnlike2(theta, x, y, xerr, yerr)
+
+def maxlike(theta0, x, y, yerr):
+    # Find the maximum likelihood values for the simple polynomial fit model.
+    chi2 = lambda *args: -2 * lnlike_simple(*args)
+    result = op.minimize(chi2, theta0[:-4], args=(x, y, yerr))
+    print("Maximum likelihood result:", result["x"])
+    # Refine our first guess for the polynomial fit
+    theta0[:-4] = result["x"]
+    return theta0
+
+def run_mcmc(x, y, xerr, yerr, theta0, nwalkers=32, nsteps=15000, burnin=2000,
+             plotdir="../im/mcmc/", clusname="test", save_results=False):
+    # Set up the sampler.
+    ndim = len(theta0)
+    pos = [theta0 + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+
+    print("Running MCMC...")
+    with Pool() as pool:
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob2, args=(x, y, xerr, yerr), pool=pool)
+        start = time.time()
+        sampler.run_mcmc(pos, nsteps, progress=True)
+        end = time.time()
+        multi_time = end - start
+        print("Multiprocessing took {0:.1f} seconds".format(multi_time))
+
+    acc_frac = np.mean(sampler.acceptance_fraction)
+    print("Mean acceptance fraction: {0:.2f}".format(acc_frac))
+
+    if acc_frac > 0.05:
+        # Get the marginalised fit parameters
+        samples = sampler.chain[:, burnin:, :].reshape((-1, ndim))
+        p0_m,p1_m,p2_m,p3_m,p4_m,p5_m,p6_m,fb_m,DG_m,sM_m,sB_m = map(lambda v: (np.round(v[1],3), 
+                                                                        np.round(v[2]-v[1],3), 
+                                                                        np.round(v[1]-v[0],3)), 
+                                                             zip(*np.percentile(samples, [16, 50, 84], axis=0)))
+        # MCMC iteration plot
+        plt.clf()
+        fig, axes = plt.subplots(ndim, 1, sharex=True, figsize=(8, 3*ndim))
+        poly_labels    = ["$p_0$", "$p_1$", "$p_2$", "$p_3$", "$p_4$", "$p_5$", "$p_6$", "$p_7$", "$p_8$"]
+        mixture_labels = ["$f_b$","$\Delta G$", "$\sigma_{MS}$", "$\sigma_{BS}$"]
+        labels         = poly_labels[:(ndim-4)] + mixture_labels
+        for ii in np.arange(ndim):
+            axes[ii].plot(sampler.chain[:, :, ii].T, color="k", alpha=0.4)
+            axes[ii].set_ylabel(labels[ii])
+        axes[ndim-1].set_xlabel("step number")
+        fig.tight_layout(h_pad=0.0)
+        fig.savefig(plotdir+"mcmc_chain_parameter_iteration_"+clusname+".png")
+        plt.close()
+
+        # Corner plot
+        fig = corner.corner(samples, labels=labels, show_titles=True, title_kwargs={"fontsize": 15})
+        fig.savefig(plotdir+"mcmc_fit_corner_"+clusname+".png")
+        plt.suptitle(clusname.replace("_", " "), fontsize=25)
+        plt.close()
+        
+        # CMD plot
+        plt.figure(figsize=(8,8))
+        plt.errorbar(x,y,xerr=xerr, yerr=yerr, ls='none', label=None, c="grey", zorder=0)
+        plt.scatter(x,y, label=None, c="k")
+        xarr = np.linspace(np.min(x),np.max(x),100)
+        # Plot the maximum likelihood result.
+        polynom = np.poly1d(theta0[:-4][::-1])
+        plt.plot(xarr, polynom(xarr), lw=2, c="grey", label="Max. likelihood fit")
+        # Plot some MCMC samples onto the data.
+        for p0,p1,p2,p3,p4,p5,p6,fb,DG,sM,sB in samples[np.random.randint(len(samples), size=30)]:
+            ms = np.poly1d([p6,p5,p4,p3,p2,p1,p0])
+            plt.plot(xarr, ms(xarr), color="b", alpha=0.2)
+            bs = np.poly1d([p6,p5,p4,p3,p2,p1,p0+DG])
+            plt.plot(xarr, bs(xarr), color="orange", alpha=0.2)
+        # Plot the best-parameter result.
+        #polynom = np.poly1d([p6_m[0],p5_m[0],p4_m[0],p3_m[0],p2_m[0],p1_m[0],p0_m[0]])
+        plt.plot(xarr, ms(xarr), c='blue', label="MCMC main sequence samples", alpha=0.2)
+        plt.plot(xarr, bs(xarr), c='orange', label="MCMC binary sequence samples", alpha=0.2)
+        plt.xlabel(r"$BP-RP$", fontsize=22)
+        plt.ylabel(r"$G$", fontsize=22)
+        plt.legend(loc="upper right", fontsize=19)
+        # Annotate name of the cluster and binary fraction
+        ax = plt.gca()
+        plt.text(0.1, 0.2, clusname.replace("_", " "), horizontalalignment='left',
+                 verticalalignment='center', transform=ax.transAxes, fontsize=22)
+        plt.text(0.1, 0.1, r"$f_b={{{0:.2f}}}^{{+{1:.2f}}}_{{-{2:.2f}}}$".format(fb_m[0],fb_m[1],fb_m[2]), 
+                 horizontalalignment='left', verticalalignment='center', transform=ax.transAxes, fontsize=22)
+        ax.invert_yaxis()
+        plt.tight_layout()
+        plt.savefig(plotdir+"cmd_fit_mcmc_"+clusname+".png")
+        plt.close()
+
+        #  save / return the results as a one-row astropy table
+        t = Table(names=('clus', 'nb_MS_members', 'MS_width', 'nsteps', 'burnin', 'acc_frac', 
+                         'fb_50', 'fb_sigu', 'fb_sigl', 'DG_50', 'DG_sigu', 'DG_sigl', 
+                         'sM_50', 'sM_sigu', 'sM_sigl', 'sB_50', 'sB_sigu', 'sB_sigl'), 
+                  dtype=('S20', 'i4', 'f4', 'i4', 'i4', 'f4', 
+                         'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4'))
+        t.add_row((clusname, len(x), np.max(x)-np.min(x), nsteps, burnin, np.round(acc_frac,2), 
+                   fb_m[0], fb_m[1], fb_m[2], DG_m[0], DG_m[1], DG_m[2], 
+                   sM_m[0], sM_m[1], sM_m[2], sB_m[0], sB_m[1], sB_m[2]))        
+        if save_results:
+            t.write(plotdir + clusname + "_mcmc_res.fits")
+        else:
+            return t
+    else:
+        print("Not converged. You need to work on your code?")
+        return None
+
+def run_pipeline_tarricq(ocdir="Tarricq_selected_members_OCs/",
+                 restabledir="data/mcmc_results_summary.fits",
+                 plotdir="im/mcmc/", ii0=0):
+    """
+    Execute the MCMC model loop for all the files in ocdir.
+    Save the tabulated results in restabledir and the plots in plotdir.
+    """
+    # Read (partial) results table if it exists
+    if ii0 !=0:
+        restable = Table.read(restabledir)
+    # Read the list of files:
+    files = sorted(os.listdir(ocdir))    
+    # Loop it away
+    for ii in np.arange(ii0, len(files)):
+        t = Table.read(ocdir + files[ii], format="ascii")
+        x    = t["bp_rp"]
+        xerr = t["bp_rp_error"]
+        y    = t["phot_g_mean_mag"]
+        yerr = t["phot_g_mean_mag_error"]
+        clusname = files[ii][:-27]
+
+        print("No.", ii, ":", clusname, "(", len(t), "stars )")
+
+        # Initial guess for ML fit
+        theta0 = [0.6, 5.67, -0.66, -0.32, 0.09, 0., 0., 0.2, -0.7, 0.05, 0.05]
+        Nfit   = len(theta0)
+        # First find the maximum likelihood values for the simple fit model.
+        theta0 = maxlike(theta0, x, y, yerr)
+        # Now run MCMC
+        result = run_mcmc(x, y, xerr, yerr, theta0, nwalkers=32, nsteps=15000, burnin=2000,
+                          plotdir=plotdir, clusname=clusname, save_results=False)
+        # Save the results
+        if ii == 0:
+            restable = result
+        else:
+            if result == None:
+                pass
+            else:
+                restable = vstack([restable, result])
+        restable.write(restabledir, overwrite=True)
+    
+def run_pipeline_cantat(ocdir="data/Cantat_selected_members_OCs_younger50Myr/",
+                 restabledir="data/mcmc_results_cantat_summary.fits",
+                 plotdir="im_mcmc/im_mcmc_cantat/", ii0=0):
+    """
+    Execute the MCMC model loop for all the files in ocdir.
+    Save the tabulated results in restabledir and the plots in plotdir.
+    """
+    # Read (partial) results table if it exists
+    if ii0 !=0:
+        restable = Table.read(restabledir)
+    # Read the list of files:
+    files = sorted(os.listdir(ocdir))    
+    # Loop it away
+    for ii in np.arange(ii0, len(files)):
+        t = Table.read(ocdir + files[ii], format="ascii")
+        x    = t["BP-RP"]
+        xerr = np.sqrt( (2.5 * np.log10(1 + 1./t["phot_bp_mean_flux_over_error"]))**2. + 
+                        (2.5 * np.log10(1 + 1./t["phot_rp_mean_flux_over_error"]))**2. )
+        y    = t["Gmag"]
+        yerr = 2.5 * np.log10(1 + 1./t["phot_g_mean_flux_over_error"])
+        clusname = files[ii][:-27]
+
+        print("No.", ii, ":", clusname, "(", len(t), "stars )")
+
+        # Initial guess for ML fit
+        theta0 = [0.6, 5.67, -0.66, -0.32, 0.09, 0., 0., 0.2, -0.7, 0.05, 0.05]
+        Nfit   = len(theta0)
+        # First find the maximum likelihood values for the simple fit model.
+        theta0 = maxlike(theta0, x, y, yerr)
+        # Now run MCMC
+        result = run_mcmc(x, y, xerr, yerr, theta0, nwalkers=32, nsteps=15000, burnin=2000,
+                          plotdir=plotdir, clusname=clusname, save_results=False)
+        # Save the results
+        if ii == 0:
+            restable = result
+        else:
+            if result == None:
+                pass
+            else:
+                restable = vstack([restable, result])
+        restable.write(restabledir, overwrite=True)
+    
+def run_pipeline_sims(ocdir="data/GOG_Simulations/Simulation_selected_members_OCs_0/",
+                      restabledir="data/mcmc_results_summary_gausspriors_simulation_0.fits",
+                      plotdir="im_mcmc/mcmc_gausspriors_simulation/", ii0=0):
+    """
+    Execute the MCMC model loop for all the files in ocdir.
+    Save the tabulated results in restabledir and the plots in plotdir.
+    """
+    # Read (partial) results table if it exists
+    if ii0 !=0:
+        restable = Table.read(restabledir)
+    # Read the list of files:
+    files = sorted(os.listdir(ocdir))    
+    # Loop it away
+    for ii in np.arange(ii0, len(files)):
+        t = Table.read(ocdir + files[ii], format="ascii")
+        x    = t["BP-RP"]
+        xerr = Table.Column([ t["sigBp"][ii]+t["sigRp"][ii] for ii in np.arange(0, len(t))])
+        y    = t["G"]
+        yerr = t["sigG"]
+        clusname = files[ii][:-25]
+
+        print("No.", ii, ":", clusname, "(", len(t), "stars )")
+
+        # Initial guess for ML fit
+        theta0 = [0.6, 5.67, -0.66, -0.32, 0.09, 0., 0., 0.2, -0.7, 0.05, 0.05]
+        Nfit   = len(theta0)
+        # First find the maximum likelihood values for the simple fit model.
+        theta0 = maxlike(theta0, x, y, yerr)
+        # Now run MCMC
+        result = run_mcmc(x, y, xerr, yerr, theta0, nwalkers=32, nsteps=15000, burnin=2000,
+                          plotdir=plotdir, clusname=clusname, save_results=False)
+        # Save the results
+        if ii == 0:
+            restable = result
+        else:
+            if result == None:
+                pass
+            else:
+                restable = vstack([restable, result])
+        restable.write(restabledir, overwrite=True)
+
+if __name__ == "__main__":
+    # Run the pipeline for the Tarricq+2021 clusters
+    run_pipeline_tarricq(ocdir="data/Tarricq_selected_members_OCs/",
+                 restabledir="data/mcmc_results_tarricq_summary.fits",
+                 plotdir="im_mcmc/im_mcmc_tarricq/", ii0=0)
